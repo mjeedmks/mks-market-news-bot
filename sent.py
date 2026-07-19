8ab88796a8f96aab60f16acbb6d06eb24a28b3e5
mjeedmks <qazzas5u@gmail.com>
2026-07-20 00:36:26 +0300

    Add functions to load and save JSON data

--- sent.py
+++ sent.py
@@ -1,1 +1,17 @@
+import json
+import os
 
+FILE = "sent.json"
+
+
+def load():
+    if not os.path.exists(FILE):
+        return set()
+
+    with open(FILE, "r") as f:
+        return set(json.load(f))
+
+
+def save(data):
+    with open(FILE, "w") as f:
+        json.dump(list(data), f)

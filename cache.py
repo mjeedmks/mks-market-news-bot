1be956b43a5a57677922ee3e108fb2cd9815a52e
mjeedmks <qazzas5u@gmail.com>
2026-07-19 23:44:31 +0300

    Update cache.py
    Implement a simple in-memory cache with expiration.

--- cache.py
+++ cache.py
@@ -1,1 +1,22 @@
+import time
 
+CACHE = {}
+
+CACHE_TIME = 60 * 60 * 2  # ساعتان
+
+
+def get(key):
+    if key not in CACHE:
+        return None
+
+    value, created = CACHE[key]
+
+    if time.time() - created > CACHE_TIME:
+        del CACHE[key]
+        return None
+
+    return value
+
+
+def set(key, value):
+    CACHE[key] = (value, time.time())
